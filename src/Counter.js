9b4cc220675c15f3b01bbeddc85946e2d07d765e
OhHalfmoon <sangoh7395@gmail.com>
2023-05-09 15:46:43 +0900

    vsc 커밋

---
+++ src/Counter.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     number:0,
+  //     fixedNumber: 0,
+  //   };
+  // }
+  state = { number: 0, fixedNumber: 0};
+  render() {
+    const {number, fixedNumber } = this.state;
+    return (
+      <div>
+        <h1>{number}</h1>
+        <h2>{fixedNumber}</h2>
+        <button
+        //onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정
+        //this.setState를 사용하여 state에 새로운 값을 넣을 수 있음
+          onClick={() => {          
+          //   this.setState((prevState) => {
+          //     return {number: prevState.number + 1}
+          //   });
+          //   this.setState((prevState) => {
+          //     return {number: prevState.number + 1}
+          //   });              
+          // }}
+            this.setState({ number: number + 1}, () => {
+              console.log("방금 setState가 호출");
+              console.log(this.state);
+            });
+          }}
+        >
+         + 1 
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
